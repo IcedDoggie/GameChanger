1dab40de4b5a0ccf98bf5f8a7e28c926f111acaf
iceboy95 <hqkhor95@gmail.com>
2016-08-04 21:52:32 +0800

    map coordinates can be read

--- Game Changer (NEW)/SelectTile.cs
+++ Game Changer (NEW)/SelectTile.cs
@@ -9,19 +9,23 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Content;
+using Nez.AI.Pathfinding;
 
 namespace Game_Changer__NEW_
 {
-    public class SelectTile : RenderableComponent, IUpdatable
+    public class SelectTile : Component, IUpdatable
     {
         TiledMap tiledmap;
         Point location;
+
+
 
         public SelectTile(TiledMap ref_tiledmap)
         {
             location = new Point(0,0);
             tiledmap = ref_tiledmap;
             var layer = tiledmap.getLayer<TiledTileLayer>("maplayer");
+
         }
 
 
@@ -33,16 +37,6 @@ namespace Game_Changer__NEW_
                 location = tiledmap.worldToTilePosition(Input.mousePosition);
                 System.Diagnostics.Debug.WriteLine(location);
             }
-
-        }
-
-        public override void render(Graphics graphics, Camera camera)
-        {
-            //    //throw new NotImplementedException();
-            System.Diagnostics.Debug.WriteLine("hello");
-            camera = null;
-            graphics = null;
-            return;
 
         }
 
