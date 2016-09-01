d8e6851956486679bcba31efaae96c18a60c55f9
iceboy95 <hqkhor95@gmail.com>
2016-09-02 00:59:07 +0800

    the stats now appear in corner

--- Game Changer (NEW)/Controlpoint.cs
+++ Game Changer (NEW)/Controlpoint.cs
@@ -29,6 +29,8 @@ namespace Game_Changer__NEW_
         Point mousePoint;
         public int tiledX;
         public int tiledY;
+        public int tiledTempX;
+        public int tiledTempY;
         public bool flagForComponent = false;
         
 
@@ -48,14 +50,29 @@ namespace Game_Changer__NEW_
             mousePoint = new Point(0, 0);
             tiledmap = ref_tiledmap;
 
-            tiledX = tiledmap.tileWidth;
-            tiledY = tiledmap.tileHeight;
 
-            tiledX = tiledX - tiledmap.tileWidth;
-            tiledY = tiledY - tiledmap.tileHeight;
+
         }
         void IUpdatable.update()
         {
+            tiledX = tiledmap.widthInPixels;
+            tiledY = tiledmap.heightInPixels;
+
+            Vector2 a = cpEntity.transform.position;
+
+            int aTemp = tiledmap.worldToTilePositionX(a.X);
+            int bTemp = tiledmap.worldToTilePositionY(a.Y);
+            tiledTempX = aTemp * 35;
+            tiledTempY = bTemp * 30;
+
+
+
+            tiledX = (tiledX - tiledmap.widthInPixels) - tiledTempX + 35;
+            tiledY = (tiledY - tiledmap.heightInPixels) - tiledTempY;
+
+            System.Diagnostics.Debug.WriteLine(tiledX);
+            System.Diagnostics.Debug.WriteLine(tiledY);
+
             entityLocation = tiledmap.worldToTilePosition(cpEntity.transform.position);
             mousePoint = tiledmap.worldToTilePosition(Input.mousePosition);
             //System.Diagnostics.Debug.WriteLine(entityLocation);
@@ -74,17 +91,9 @@ namespace Game_Changer__NEW_
             {
                 flagForComponent = false;
                 cpEntity.removeComponent(statsText);   
-                //cpEntity.removeComponent(statsText);
-                //this.factionName = "";
-                //System.Diagnostics.Debug.WriteLine("CP Not Found");
             }
             
         }
 
-        //void statsUpdate()
-        //{
-        //    statsTimer = new Timer(updateInterval);
-        //    statsTimer.Elapsed += statsTimer_el
-        //}
     }
 }
